9522f267eca060c8c225f40fb6264c2ad12570be
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-06-14 15:00:12 +0200

    feat end of step 1

--- src/main/java/org/lessons/java/spring/exercise/best_of_the_year/controller/HomeController.java
+++ src/main/java/org/lessons/java/spring/exercise/best_of_the_year/controller/HomeController.java
@@ -1,5 +1,38 @@
 package org.lessons.java.spring.exercise.best_of_the_year.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
+
+    @GetMapping("/")
+    public String Home(Model model, String name) {
+        model.addAttribute("name", "Luca");
+        return "index";
+    }
+
+    // @GetMapping("/saluti")
+    // public String Saluti(Model model, @RequestParam(name = "name") String name) {
+    // final String formattedName = name.substring(0, 1).toUpperCase()
+    // + name.substring(1, name.length()).toLowerCase();
+    // model.addAttribute("name", formattedName);
+    // model.addAttribute("time", LocalTime.now());
+    // model.addAttribute("email", name + "@gmail.com");
+    // return "saluti";
+    // }
+
+    // @GetMapping("/colore")
+    // public String Colore(Model model, @RequestParam(name = "color") String color)
+    // {
+
+    // model.addAttribute("color", color);
+
+    // return "colore";
+    // }
 
 }
